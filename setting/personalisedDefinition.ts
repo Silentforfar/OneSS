45d06dd7a5a6a07eb5d761b612b068df6fa30db6
Silentforfar <101911556+Silentforfar@users.noreply.github.com>
2022-03-20 00:35:08 +0800

    Update personalisedDefinition.ts

--- setting/personalisedDefinition.ts
+++ setting/personalisedDefinition.ts
@@ -1,13 +1,13 @@
 const personalisedDefinition = {
     index: {
-        backgroundImage: '/api/bingPic',
-        title: 'OneSS',
+        backgroundImage: 'https://tenapi.cn/acg',
+        title: 'WELCOME',
         introduction: 'Make your Onedrive work like OSS'
     },
     footer:{
-        name:'Tualin14',
-        link:'https://github.com/Tualin14',
-        bilibili:'https://space.bilibili.com/403504801'
+        name:'DONGMMM',
+        link:' ',
+        bilibili:' '
     }
 }
 
